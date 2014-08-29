4b5503ef84785b15fec0b7861af09902192cc864
Keith Perfetti <perfetti@hb-networks.com>
2014-08-28 22:36:35 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+	<head>
+	  <meta charset="utf-8">
+
+	  <title>Myspace NYC</title>
+	  <meta name="description" content="The HTML5 Herald">
+	  <meta name="author" content="SitePoint">
+
+	  <link rel="stylesheet" href="styles/main.css?v=1.0">
+	  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	  <script src="/js/main.js" > </script>
+
+	  <!--[if lt IE 9]>
+	  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	  <![endif]-->
+	</head>
+
+	<body>
+
