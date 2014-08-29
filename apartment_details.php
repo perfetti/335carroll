0ec3d2b15678acb8b8c8dc07eeb3a5fb45e1adbd
indexr <tech@indexable.net>
2014-08-29 10:30:12 -0400

    Update apartment_details.php
    Added copy

--- apartment_details.php
+++ apartment_details.php
@@ -14,6 +14,61 @@
 	
 	<!-- related details -->
 	<article id="overview" class="details active">
+		This five-story, 30-unit luxury building located on treelined streets flanked by Brooklyn brownstone and townhouses in the heart of historic Carroll Gardens is where old world charm meets modern convenience. Originally built in 1931, no expense was spared in the full-gut rehab to offer premium finishes and state-of-the-art amenities for true comfort you can call home immediately upon move in. Pet friendly. Guarantors accepted.<br />
+		Call or email for a viewing today.
+
+	</article>
+	<article id="amenities" class="details">
+		This elevator building features balconied units - some with spectacular downtown Manhattan views -  with ensuite laundry, parking garage, bike room, state-of-the-art lounge, and fitness center. Each apartment was fitted with premium Bosch and GE stainless steel appliances, as well as one of a kind custom designed lighting fixtures, and ensuite climate control.
+	</article>
+	<article id="neighborhood" class="details">
+		Prized for its quick commute to downtown Manhattan and Park Slope, Carroll Gardens is a destination neighborhood boasting of every essential services, Smith Street's restaurant row and Court Street's myriad boutiques as well as Brooklyn’s first Whole Foods located minutes away in Gowanus and Trader Joe's at Court St and Atlantic Av. 
+<ul>
+	<li>Nestled in the heart of Brooklyn, at the intersection of tony Carroll Gardens between Hoyt and Bond, bordering edgy Gowanus, flanked by Cobble Hill, Red Hook, and Park Slope 	</li>
+
+<li>1.5 blocks from F and G lines on Carroll Street, the only station with a built-in branch outlet of the famed Momofuku Milk Bar </li>
+<li>2 blocks from the Union Street R Train stop </li>
+<li>5-10 minutes from the new Whole Foods on 3rd St and 3rd Av in Gowanus</li>
+<li>2 blocks from the5,000- square foot Royal Palms shuffleboard Club, an entertainment and dining, Lavender Lake and Dinosaur BBQ.</li>
+<li>Walk up and down Smith and Court and find everything you need from laundry and dry cleaners</li>	
+<li>Two movies theaters  </li>
+<li>1.5 blocks from Carrol Gardens public park</li>
+</ul>	
+
+
+	</article>
+
+
+</section>
+<script>
+ 	window.myspace.enableDetailsSwapEffect();
+ 	window.myspace.enableSidebarEffect();
+</script>
+
+<section id="apartment_photos">
+<!-- FIXME 
+		PUT A BUNCH OF PICTURES IN HERE it'll scroll -->
+		 <img src="http://335carroll.com/images/" alt="some_text">
+		  <img src="http://335carroll.com/images/" alt="some_text">
+		   <img src="http://335carroll.com/images/" alt="some_text">
+		    <img src="http://335carroll.com/images/" alt="some_text">
+		     <img src="http://335carroll.com/images/" alt="some_text">
+<?php $include_sidebar = true ?>
+<?php include 'header.php' ?>
+
+<nav id="sidebar">
+
+</nav>
+
+<section id="apartment_details">
+	<ul class="cf">
+		<li data-detail-select="overview" class="active"></li>
+		<li data-detail-select="amenities" >Amenities</li>
+		<li data-detail-select="neighborhood" >Neighborhood</li>
+	</ul>
+	
+	<!-- related details -->
+	<article id="overview" class="details active">
 		details
 	</article>
 	<article id="amenities" class="details">
@@ -33,6 +88,9 @@
 <section id="apartment_photos">
 	<!-- FIXME 
 		PUT A BUNCH OF PICTURES IN HERE it'll scroll -->
+</section>
+
+<?php include 'footer.php' ?>
 </section>
 
 <?php include 'footer.php' ?>
